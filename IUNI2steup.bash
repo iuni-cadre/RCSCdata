092fd31cc5142eb601a99a3e2872c561fff2d336
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2020-05-24 12:13:39 -0400

    Update IUNI2steup.bash

--- IUNI2steup.bash
+++ IUNI2steup.bash
@@ -14,16 +14,9 @@ spark-shell
 :q
 
 ###### Setting up jupyter server #####
-
 pip install --user Toree
 jupyter toree install --spark_home=$SPARK_HOME --user --spark_opts=" --master yarn  --packages com.databricks:spark-xml_2.11:0.5.0,graphframes:graphframes:0.7.0-spark2.4-s_2.11 --driver-memory 8G --executor-memory 14G --executor-cores 7 --conf spark.driver.maxResultSize=8g"
 jupyter kernelspec list
-
-hadoop fs -rm -R URI
-hdfs dfs -mkdir /data1
-hdfs dfs -copyFromLocal /wos_17_18/ /data1/WoSraw/
-hdfs dfs -ls /
-hdfs dfs -copyToLocal <hdfs_input_file_path> <output_path>
 
 jupyter serverextension enable --py jupyterlab
 jupyter notebook --no-browser --port=8000 --ip=149.165.230.163 ## With the ip option, it is only accessible to other iuni nodes
@@ -38,3 +31,10 @@ sqlContext.sql("set spark.sql.caseSensitive=true")
 
 ##### next output will give you spark UI for tracking your jobs, only only accessible to other iuni nodes
 spark.sparkContext.uiWebUrl
+
+##### for interacting with the native HDFS on the cluster
+hadoop fs -rm -R URI
+hdfs dfs -mkdir /data1
+hdfs dfs -copyFromLocal /wos_17_18/ /data1/WoSraw/
+hdfs dfs -ls /
+hdfs dfs -copyToLocal <hdfs_input_file_path> <output_path>
